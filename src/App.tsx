dca9d662cb84ccd3c29ce413a38a2eb43e91974d
Tomoya Ito <tomoyaito199507265724@gmail.com>
2023-12-24 09:19:20 +0900

    components作成完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter} from "react-router-dom";
+
 import theme from './theme/theme';
+
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
+      <BrowserRouter>
         <Button colorScheme='teal'>ボタン</Button>
         <p>あああ</p>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
