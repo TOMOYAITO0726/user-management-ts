339d831c27e274365bec7db54df00fd5c1c2c02f
Tomoya Ito <tomoyaito199507265724@gmail.com>
2023-12-27 12:42:38 +0900

    ログイン画面完成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,18 @@
 import { memo, VFC } from "react";
+import {Box, Button, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
 
 export const Login:VFC = memo(() => {
-    return<p>ログインページです</p>;
+    return(
+        <Flex align="center" justify="center" height="100vh" shadow="md">
+            <Box bg="white" w="sm" p={4} borderRadius="md">
+                <Heading as="h1" size="lg" textAlign="center" >ユーザー管理アプリ</Heading>
+                <Divider my={4}/>
+                <Stack spacing={6} py={4} px={10}>
+                <Input placeholder="ユーザーID"/>
+                <Button bg="teal.400" color="white" _hover={{ opacity:0.8}} >ログイン</Button>
+                </Stack>
+            </Box>
+        </Flex>
+    );
 });
+//stackは囲った7要素の間隔を等間隔にしてくれる
