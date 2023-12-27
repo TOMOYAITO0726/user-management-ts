65abd7a2734db64da90771df1a368afdcb956e23
Tomoya Ito <tomoyaito199507265724@gmail.com>
2023-12-27 14:21:11 +0900

    PrimaryButton分割

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { memo, VFC } from "react";
 import {Box, Button, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login:VFC = memo(() => {
     return(
@@ -9,10 +10,10 @@ export const Login:VFC = memo(() => {
                 <Divider my={4}/>
                 <Stack spacing={6} py={4} px={10}>
                 <Input placeholder="ユーザーID"/>
-                <Button bg="teal.400" color="white" _hover={{ opacity:0.8}} >ログイン</Button>
+                <PrimaryButton>ログイン</PrimaryButton>
                 </Stack>
             </Box>
         </Flex>
     );
 });
-//stackは囲った7要素の間隔を等間隔にしてくれる
+//stackは囲った要素の間隔を等間隔にしてくれる
