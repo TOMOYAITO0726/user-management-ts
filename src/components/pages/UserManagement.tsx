935159f0718fcdb554789e1a98dba6e940cc4de9
Tomoya Ito <tomoyaito199507265724@gmail.com>
2024-01-01 21:47:10 +0900

    ユーザー一覧画面Wrap実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,14 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisims/layout/user/UserCard";
 
 export const UserManagement:VFC = memo(() => {
-    return<p>ユーザー管理ページです</p>;
+    return(
+        <Wrap p={{base: 4, md: 10}}>
+            <WrapItem>
+                <UserCard imageUrl="https://source.unsplash.com/random" userName="ユーザー1" fullName="User1"/>
+            </WrapItem>
+        </Wrap>
+    );
 });
+//読み込む度にランダムに画像が表示される
