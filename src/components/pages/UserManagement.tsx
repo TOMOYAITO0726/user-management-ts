0a9e8822ada7066b97d223d5bd5f89129b1528f6
Tomoya Ito <tomoyaito199507265724@gmail.com>
2024-01-02 10:40:29 +0900

    ユーザー一覧データ取得,spinner実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,29 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
 import { UserCard } from "../organisims/layout/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement:VFC = memo(() => {
+    const {getUsers, users, loading} = useAllUsers();
+    useEffect(()=> getUsers(), [])
     return(
+        <>
+        {loading? (
+            <Center h="100vh">
+                 <Spinner/> 
+            </Center>
+       ):(
         <Wrap p={{base: 4, md: 10}}>
-            <WrapItem>
-                <UserCard imageUrl="https://source.unsplash.com/random" userName="ユーザー1" fullName="User1"/>
+                {users.map((user)=>(
+            <WrapItem key={user.id} mx="auto">
+                <UserCard 
+                imageUrl="https://source.unsplash.com/random" 
+                userName={user.username}
+                fullName={user.name}/>
             </WrapItem>
+            ))}
         </Wrap>
+        )}
+        </>
     );
 });
-//読み込む度にランダムに画像が表示される
